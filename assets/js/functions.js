fc152c8a053ca9350a1807e956163eb9aba2f65a
Haddadi Abdraouf <68353042+Al-Wajeez@users.noreply.github.com>
2024-10-29 19:55:31 +0100

    preload

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1097,3 +1097,12 @@ var e = {
 
 };
 e.init();
+
+	$(window).on('load', function() {
+    //for use in production please remove this setTimeOut
+    setTimeout(function(){ 
+        $('.preloader').addClass('preloader-deactivate');
+    }, 1000);
+    //uncomment this line for use this snippet in production
+    //	$('.preloader').addClass('preloader-deactivate');
+});
